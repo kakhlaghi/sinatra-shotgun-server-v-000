77bbb3e411ad82f8b2662ede5e9751a716b93a55
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-28 21:19:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! I BUILT THIS!"
+  get '/' do
+    "STarted my server using Shotgun"
   end
 
 end
